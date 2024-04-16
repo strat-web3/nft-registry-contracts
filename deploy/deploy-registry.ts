d17dfffdf961c98c8a3a68aa2bd8245301012658
julienbrg <julien@strat.cc>
2024-04-16 12:24:38 +0200

    clean up

--- deploy/deploy-registry.ts
+++ deploy/deploy-registry.ts
@@ -26,34 +26,6 @@ export default async ({ getNamedAccounts, deployments }: any) => {
     console.log("hre.network.name:", hre.network.name)
 
     switch (hre.network.name) {
-        case "arthera-testnet":
-            // console.log(
-            //     "Basic ERC-20 token contract deployed:",
-            //     msg(registry.receipt.contractAddress)
-            // )
-
-            try {
-                // Please use `pnpm sourcify:arthera` after the deployment instead.
-
-                // console.log("\nEtherscan verification in progress...")
-                // console.log(
-                //     "\nWaiting for 6 block confirmations (you can skip this part)"
-                // )
-                // await basic.deploymentTransaction()?.wait(6)
-                // await hre.run("verify:verify", {
-                //     network: network.name,
-                //     address: basic.receipt.contractAddress,
-                //     constructorArguments: [initialMint]
-                // })
-
-                console.log(
-                    "Please use `pnpm sourcify:arthera` to verify your contract."
-                )
-            } catch (error) {
-                console.error(error)
-            }
-
-            break
         case "sepolia":
             try {
                 // console.log(
